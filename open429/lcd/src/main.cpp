d4acb7dc06a20aef03d8d81e8fb2a805496ced5d
Jean-Claude Wippler <jc@wippler.nl>
2018-10-20 12:06:07 +0200

    add simple lcd using ili9341 spi

---
+++ open429/lcd/src/main.cpp
@@ -0,0 +1,25 @@
+#include <jee.h>
+#include <jee/spi-ili9341.h>
+#include <string.h>
+
+UartBufDev< PinA<9>, PinA<10> > console;
+#include <../../common.h>
+
+SpiGpio< PinF<9>, PinF<8>, PinF<7>, SlowPin< PinC<2>, 10> > spi;
+PinD<13> wrx;
+ILI9341< decltype(spi), decltype(wrx) > lcd;
+
+int main() {
+	console.init();
+	fullSpeedClock();
+	printf("hello\n");
+
+	spi.init();
+	lcd.init();
+
+	for (int x = 0; x < 240; ++x)
+		for (int y = 0; y < 320; ++y)
+			lcd.pixel(x, y, 0xFFE0);
+
+    while (1) {}
+}
