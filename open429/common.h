7dbc260bbe4417a35841e505370b891e2ff88b02
Jean-Claude Wippler <jc@wippler.nl>
2018-10-24 16:09:39 +0200

    cleanup

--- open429/common.h
+++ open429/common.h
@@ -6,29 +6,25 @@ int printf(const char* fmt, ...) {
 }
 
 void enableClkAt160MHz () {
-    constexpr uint32_t rcc   = Periph::rcc;
-
     MMIO32(Periph::flash + 0x00) = 0x705; // flash acr, 5 wait states
-    MMIO32(rcc + 0x00) = (1<<16); // HSEON
-    while ((MMIO32(rcc + 0x00) & (1<<17)) == 0) ; // wait for HSERDY
-    MMIO32(rcc + 0x08) = (4<<13) | (5<<10) | (1<<0); // prescalers and use HSE
-    MMIO32(rcc + 0x04) = (7<<24) | (1<<22) | (0<<16) | (160<<6) | (4<<0);
-    MMIO32(rcc + 0x00) |= (1<<24); // PLLON
-    while ((MMIO32(rcc + 0x00) & (1<<25)) == 0) ; // wait for PLLRDY
-    MMIO32(rcc + 0x08) = (4<<13) | (5<<10) | (2<<0);
+    MMIO32(Periph::rcc + 0x00) = (1<<16); // HSEON
+    while ((MMIO32(Periph::rcc + 0x00) & (1<<17)) == 0) ; // wait for HSERDY
+    MMIO32(Periph::rcc + 0x08) = (4<<13) | (5<<10) | (1<<0); // prescaler w/ HSE
+    MMIO32(Periph::rcc + 0x04) = (7<<24) | (1<<22) | (0<<16) | (160<<6) | (4<<0);
+    MMIO32(Periph::rcc + 0x00) |= (1<<24); // PLLON
+    while ((MMIO32(Periph::rcc + 0x00) & (1<<25)) == 0) ; // wait for PLLRDY
+    MMIO32(Periph::rcc + 0x08) = (4<<13) | (5<<10) | (2<<0);
 }
 
 void enableClkAt180MHz () {
-    constexpr uint32_t rcc   = Periph::rcc;
-
     MMIO32(Periph::flash + 0x00) = 0x705; // flash acr, 5 wait states
-    MMIO32(rcc + 0x00) = (1<<16); // HSEON
-    while ((MMIO32(rcc + 0x00) & (1<<17)) == 0) ; // wait for HSERDY
-    MMIO32(rcc + 0x08) = (4<<13) | (5<<10) | (1<<0); // prescalers and use HSE
-    MMIO32(rcc + 0x04) = (7<<24) | (1<<22) | (0<<16) | (180<<6) | (4<<0);
-    MMIO32(rcc + 0x00) |= (1<<24); // PLLON
-    while ((MMIO32(rcc + 0x00) & (1<<25)) == 0) ; // wait for PLLRDY
-    MMIO32(rcc + 0x08) = (4<<13) | (5<<10) | (2<<0);
+    MMIO32(Periph::rcc + 0x00) = (1<<16); // HSEON
+    while ((MMIO32(Periph::rcc + 0x00) & (1<<17)) == 0) ; // wait for HSERDY
+    MMIO32(Periph::rcc + 0x08) = (4<<13) | (5<<10) | (1<<0); // prescaler w/ HSE
+    MMIO32(Periph::rcc + 0x04) = (7<<24) | (1<<22) | (0<<16) | (180<<6) | (4<<0);
+    MMIO32(Periph::rcc + 0x00) |= (1<<24); // PLLON
+    while ((MMIO32(Periph::rcc + 0x00) & (1<<25)) == 0) ; // wait for PLLRDY
+    MMIO32(Periph::rcc + 0x08) = (4<<13) | (5<<10) | (2<<0);
 }
 
 constexpr uint32_t sdram = 0xD0000000;
