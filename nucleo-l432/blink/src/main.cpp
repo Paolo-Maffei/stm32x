c5637e1a35adb6df0bd1b5abdd97a2d0a7a7d0f4
Jean-Claude Wippler <jc@wippler.nl>
2018-10-25 22:57:19 +0200

    switch to full speed @ 80 MHz

--- nucleo-l432/blink/src/main.cpp
+++ nucleo-l432/blink/src/main.cpp
@@ -4,15 +4,16 @@ UartBufDev< PinA<2>, PinA<3> > console;
 
 int printf(const char* fmt, ...) {
     va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
-	return 0;
+    return 0;
 }
 
 PinB<3> led;
 
 int main() {
     console.init();
-	enableSysTick(); // no HSE crystal
-	led.mode(Pinmode::out);
+    console.baud(115200, fullSpeedClock());
+
+    led.mode(Pinmode::out);
 
     while (1) {
         printf("%d\n", ticks);
