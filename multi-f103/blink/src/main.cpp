c26b8389486a48648087c76c1b03998e8c626037
Jean-Claude Wippler <jc@wippler.nl>
2018-11-06 21:30:37 +0100

    add blink for mini multi-function F103C8 board

---
+++ multi-f103/blink/src/main.cpp
@@ -0,0 +1,24 @@
+#include <jee.h>
+
+UartBufDev< PinA<9>, PinA<10> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+	return 0;
+}
+
+PinA<1> led;
+
+int main() {
+    console.init();
+	console.baud(115200, fullSpeedClock());
+	led.mode(Pinmode::out);
+
+    while (1) {
+        printf("%d\n", ticks);
+        led = 0;
+        wait_ms(100);
+        led = 1;
+        wait_ms(400);
+    }
+}
