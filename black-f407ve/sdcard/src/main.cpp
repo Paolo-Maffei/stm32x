9cab30ab3d3eea3685e8896503139bf734a999df
Jean-Claude Wippler <jc@wippler.nl>
2018-11-08 21:50:28 +0100

    sd card demo, just basic presence/access

---
+++ black-f407ve/sdcard/src/main.cpp
@@ -0,0 +1,34 @@
+#include <jee.h>
+#include <jee/spi-sdcard.h>
+
+UartBufDev< PinA<9>, PinA<10> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+	return 0;
+}
+
+PinA<6> led;
+
+SpiGpio< PinD<2>, PinC<8>, PinC<12>, PinC<11> > spi;
+SdCard< decltype(spi) > sd;
+
+int main() {
+    console.init();
+	console.baud(115200, fullSpeedClock()/2);
+    wait_ms(1000);
+    printf("\n-------------------------------------------------------------\n");
+	led.mode(Pinmode::out);
+
+    spi.init();
+    if (sd.init())
+        printf("sdhc %d\n", sd.sdhc);
+
+    while (1) {
+        printf("%d\n", ticks);
+        led = 0;
+        wait_ms(100);
+        led = 1;
+        wait_ms(400);
+    }
+}
