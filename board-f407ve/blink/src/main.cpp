f522de2bd37e0c4842cb9206809e5cef490a3f63
Jean-Claude Wippler <jc@wippler.nl>
2018-11-08 10:55:16 +0100

    add f407ve blink demo, with led on PC2

---
+++ board-f407ve/blink/src/main.cpp
@@ -0,0 +1,24 @@
+#include <jee.h>
+
+UartBufDev< PinA<9>, PinA<10> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+	return 0;
+}
+
+PinC<2> led;
+
+int main() {
+    console.init();
+	console.baud(115200, fullSpeedClock()/2);
+	led.mode(Pinmode::out);
+
+    while (1) {
+        printf("%d\n", ticks);
+        led = 1;
+        wait_ms(100);
+        led = 0;
+        wait_ms(400);
+    }
+}
