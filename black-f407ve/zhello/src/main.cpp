d7494a160a773c991797d6f128ce788211a537ca
Jean-Claude Wippler <jc@wippler.nl>
2018-11-09 22:49:19 +0100

    cleanup

--- black-f407ve/zhello/src/main.cpp
+++ black-f407ve/zhello/src/main.cpp
@@ -1,12 +1,4 @@
-/* Example program using z80emu to run the zexall tests. This will 
- * check if the Z80 is correctly emulated.
- *
- * Copyright (c) 2012, 2016 Lin Ke-Fong
- * Copyright (c) 2012 Chris Pressey
- * Ported to STM32 by jcw, Nov 2018.
- *
- * This code is free, do whatever you want with it.
- */
+// Run a minimal "hello" example with z80 emu, to illustrate running z80 code
 
 #include <jee.h>
 #include <string.h>
@@ -22,72 +14,25 @@ const uint8_t rom [] = {
 
 UartBufDev< PinA<9>, PinA<10> > console;
 
-int printf(const char* fmt, ...) {
-    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
-	return 0;
-}
+ZEXTEST zex;
 
-PinA<6> led;
-
-/* Emulate "zexdoc.com" or "zexall.com". */
-
-static void emulate (const void* rom, int len)
-{
-    static ZEXTEST	context;
-    memcpy(context.memory + 0x100, rom, len);
-
-    /* Patch the memory of the program. Reset at 0x0000 is trapped by an
-     * OUT which will stop emulation. CP/M bdos call 5 is trapped by an IN.
-     * See Z80_INPUT_BYTE() and Z80_OUTPUT_BYTE() definitions in z80user.h.
-     */
-
-    context.memory[0] = 0xd3;       /* OUT N, A */
-    context.memory[1] = 0x00;
-
-    context.memory[5] = 0xdb;       /* IN A, N */
-    context.memory[6] = 0x00;
-    context.memory[7] = 0xc9;       /* RET */
-
-    context.is_done = 0;
-
-    /* Emulate. */
-
-    Z80Reset(&context.state);
-    context.state.pc = 0x100;
-    do {
-        led.toggle();
-        Z80Emulate(&context.state, 4000000, &context);
-    } while (!context.is_done);
-    led = 0;
-}
-
-/* Emulate CP/M bdos call 5 functions 2 (output character on screen) and 9
- * (output $-terminated string to screen).
- */
-
-void SystemCall (ZEXTEST *zextest)
-{
-    if (zextest->state.registers.byte[Z80_C] == 2)
-        printf("%c", zextest->state.registers.byte[Z80_E]);
-    else if (zextest->state.registers.byte[Z80_C] == 9) {
-        int     i, c;
-        for (i = zextest->state.registers.word[Z80_DE], c = 0; 
-                zextest->memory[i] != '$';
-                i++) {
-
-            printf("%c", zextest->memory[i & 0xffff]);
-        }
-    }
+void SystemCall (ZEXTEST*) {
+    for (int i = zex.state.registers.word[Z80_DE]; zex.memory[i] != '$'; i++)
+        console.putc(zex.memory[i & 0xffff]);
 }
 
 int main() {
     console.init();
-    console.baud(115200, fullSpeedClock()/2);
-    led.mode(Pinmode::out);
 
-    uint32_t start = ticks;
-    emulate(rom, sizeof rom);
-    printf("Emulating zexall took %d ms.\n", ticks - start);
+    memcpy(zex.memory + 0x100, rom, sizeof rom);
+
+    zex.is_done = 0;
+
+    Z80Reset(&zex.state);
+    zex.state.pc = 0x100;
+    do
+        Z80Emulate(&zex.state, 4000000, &zex);
+    while (!zex.is_done);
 
     while (1) {}
 }
