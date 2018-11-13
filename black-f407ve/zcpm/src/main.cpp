e030d707240cc6884f8c44e5f3fc5250e5c82e6f
Jean-Claude Wippler <jc@wippler.nl>
2018-11-13 17:01:22 +0100

    test mapping a drive to the SD card

--- black-f407ve/zcpm/src/main.cpp
+++ black-f407ve/zcpm/src/main.cpp
@@ -37,9 +37,35 @@ SpiWear< decltype(spiFlash), PinA<6> > spiWear;
 SpiGpio< PinD<2>, PinC<8>, PinC<12>, PinC<11> > spi2;
 SdCard< decltype(spi2) > sdCard;
 FatFS< decltype(sdCard) > fatFs;
-FileMap< decltype(fatFs), 9 > disks [] = {fatFs,fatFs}; // TODO yucky init
 
-ZEXTEST zex;
+// TODO yucky init
+typedef FileMap< decltype(fatFs), 9 > DiskMap;
+DiskMap disks [] = {fatFs,fatFs};
+DiskMap* currDisk;
+int currSect;
+bool currDirty;
+uint8_t currBuf [512];
+
+void* reBlock128 (DiskMap* dmp =0, int blk =0, bool dirty =false) {
+    int sect = blk / 4;  // CP/M blocks are 128b, SD card sectors are 512b
+    if (dmp != currDisk || sect != currSect) {
+        if (currDisk != 0 && currDirty)
+            if (!currDisk->writeSect(currSect, currBuf))
+                printf("WERR: disk %d sect %d blk %d\n", dmp-disks, sect, blk);
+        currDisk = dmp;
+        currSect = sect;
+        currDirty = false;
+        if (currDisk != 0)
+            if (!currDisk->readSect(currSect, currBuf))
+                printf("RERR: disk %d sect %d blk %d\n", dmp-disks, sect, blk);
+    }
+    currDirty |= dirty;
+    return currBuf + (blk*128) % 512;
+}
+
+const uint8_t skewMap [] = {
+    1,7,13,19,25,5,11,17,23,3,9,15,21,2,8,14,20,26,6,12,18,24,4,10,16,22
+};
 
 void SystemCall (ZEXTEST* z, int req) {
 
@@ -61,25 +87,49 @@ void SystemCall (ZEXTEST* z, int req) {
             break;
         case 4: { // read
 #if 0
-148 FEE7  3A 0004     read:   ld a,(usrdrv)
-149 FEEA  06 01               ld b,1
-150 FEEC  ED 5B FF88          ld de,(seksat)
-151 FEF0  2A FF8A             ld hl,(dmaadr)
-152 FEF3  DB 04               in a,(4)
-153 FEF5  C9          	ret
+read:   ld a,(sekdrv)
+        ld b,1
+        ld de,(seksat)
+        ld hl,(dmaadr)
+        in a,(4)
+	ret
 #endif
             uint8_t sec = DE, trk = DE >> 8, dsk = A;
-            int blk = (256*1024/128) * dsk + 26 * trk + sec;
-            for (int i = 0; i < B; ++i)
-                spiWear.read128(blk + i, z->memory + HL + 128 * i);
+            //printf("r128: cnt %d dsk %d trk %d sec %d -> %d\n",
+            //        B, dsk, trk, sec, skewMap[sec]);
+            if (dsk > 0)
+                sec = skewMap[sec]-1;
+            int blk = 26 * trk + sec;
+            for (int i = 0; i < B; ++i) {
+                void* ptr = z->memory + HL + 128 * i;
+                if (dsk > 0)
+                    memcpy(ptr, reBlock128(&disks[dsk-1], blk+i, false), 128);
+                else
+                    spiWear.read128((256*1024/128)*dsk + blk + i, ptr);
+            }
+            if (dsk > 10) {
+                uint8_t* ptr = (uint8_t*) reBlock128(&disks[dsk-1], blk, false);
+                for (int i = 0; i < 128; ++i) {
+                    printf("%c", ' ' <= ptr[i] && ptr[i] <= '~' ? ptr[i] : '.');
+                    if (i % 32 == 31)
+                        printf("\n");
+                }
+            }
             A = 0;
             break;
         }
         case 5: { // write
             uint8_t sec = DE, trk = DE >> 8, dsk = A;
-            int blk = (256*1024/128) * dsk + 26 * trk + sec;
-            for (int i = 0; i < B; ++i)
-                spiWear.write128(blk + i, z->memory + HL + 128 * i);
+            if (dsk > 0)
+                sec = skewMap[sec]-1;
+            int blk = 26 * trk + sec;
+            for (int i = 0; i < B; ++i) {
+                void* ptr = z->memory + HL + 128 * i;
+                if (dsk > 0)
+                    memcpy(reBlock128(&disks[dsk-1], blk+i, true), ptr, 128);
+                else
+                    spiWear.write128((256*1024/128)*dsk + blk + i, ptr);
+            }
             A = 0;
             break;
         }
@@ -89,9 +139,11 @@ void SystemCall (ZEXTEST* z, int req) {
     }
 }
 
+ZEXTEST zex;
+
 int main() {
     console.init();
-    console.baud(115200, fullSpeedClock()/2);
+    enableSysTick(); //console.baud(115200, fullSpeedClock()/2);
     printf("\r\n");
 
     key0.mode(Pinmode::in_pullup); // inverted logic
@@ -177,8 +229,15 @@ int main() {
 
     Z80Reset(&zex.state);
 
-    while (!zex.is_done)
+    do {
         Z80Emulate(&zex.state, 4000000, &zex);
+
+        static uint32_t last;
+        if (last != ticks/3000) {
+            last = ticks/3000; // approx every 3s ...
+            reBlock128();      // ... flush pending changes to SD card
+        }
+    } while (!zex.is_done);
 
     while (1) {}
 }
