5188082a491f6d89ea679e146f0e6e0e09cf44fd
Jean-Claude Wippler <jc@wippler.nl>
2018-11-27 15:37:22 +0100

    drop button, add simple dummy loop timing test

--- diymore-f407vg/blink/src/main.cpp
+++ diymore-f407vg/blink/src/main.cpp
@@ -2,33 +2,37 @@
 
 #include <jee.h>
 
-UartBufDev< PinA<9>, PinA<10> > console;
+UartDev< PinA<9>, PinA<10> > console;
 
 int printf(const char* fmt, ...) {
     va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
 	return 0;
 }
 
-PinD<15> button;
 PinE<0> led;
+
+SysTick<168000000> tick;
 
 int main() {
     console.init();
     console.baud(115200, fullSpeedClock()/2);
-    printf("\r\n");
+    printf("\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
 
-    printf("flash %d kb\n", MMIO16(0x1FFF7A22));
-    printf("h/w id %08x %08x %08x\n",
-        MMIO32(0x1FFF7A10), MMIO32(0x1FFF7A14), MMIO32(0x1FFF7A18));
-
-    button.mode(Pinmode::in_pullup);
     led.mode(Pinmode::out);
 
+    // dummy loop timing test
+    uint32_t t = tick.micros();
+    for (int i = 0; i < 1000000; ++i) __asm("");
+    uint32_t ns = tick.micros() - t;
+    uint32_t ns2 = 1000000000/168000;
+    printf("%d ps, %d ps, %d x1000, %d MHz\n",
+            ns, ns2, 1000*ns/ns2, 1000000/ns);
+
     while (1) {
-        printf("%d %d\n", ticks, (int) button);
+        printf("%d\n", ticks);
         led = 0;
         wait_ms(100);
         led = 1;
-        wait_ms(400);
+        wait_ms(900);
     }
 }
