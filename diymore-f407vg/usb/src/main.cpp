b2032d98d413b2da06725563d978d1672532159b
Jean-Claude Wippler <jc@wippler.nl>
2018-11-28 19:28:18 +0100

    cleanup

--- diymore-f407vg/usb/src/main.cpp
+++ diymore-f407vg/usb/src/main.cpp
@@ -57,92 +57,21 @@ namespace USB {
     };
 
     const uint8_t confDesc [] = { // total length = 67 bytes
-        // USB Configuration Descriptor
-        9,   // bLength: Configuration Descriptor size
-        0x02, // USB_CONFIGURATION_DESCRIPTOR_TYPE
-        67,  // VIRTUAL_COM_PORT_SIZ_CONFIG_DESC
-        0,
-        2,   // bNumInterfaces: 2 interface
-        1,   // bConfigurationValue
-        0,   // iConfiguration
-        0xC0, // bmAttributes: self powered
-        0x32, // MaxPower 0 mA
-        // Interface Descriptor
-        9,   // bLength: Interface Descriptor size
-        0x04, // USB_INTERFACE_DESCRIPTOR_TYPE
-        0x00, // bInterfaceNumber: Number of Interface
-        0x00, // bAlternateSetting: Alternate setting
-        0x01, // bNumEndpoints: One endpoints used
-        0x02, // bInterfaceClass: Communication Interface Class
-        0x02, // bInterfaceSubClass: Abstract Control Model
-        0x01, // bInterfaceProtocol: Common AT commands
-        0x00, // iInterface:
-        // Header Functional Descriptor
-        5,   // bLength: Endpoint Descriptor size
-        0x24, // bDescriptorType: CS_INTERFACE
-        0x00, // bDescriptorSubtype: Header Func Desc
-        0x10, // bcdCDC: spec release number
-        0x01,
-        // Call Management Functional Descriptor
-        5,   // bFunctionLength
-        0x24, // bDescriptorType: CS_INTERFACE
-        0x01, // bDescriptorSubtype: Call Management Func Desc
-        0x00, // bmCapabilities: D0+D1
-        0x01, // bDataInterface: 1
-        // ACM Functional Descriptor
-        4,   // bFunctionLength
-        0x24, // bDescriptorType: CS_INTERFACE
-        0x02, // bDescriptorSubtype: Abstract Control Management desc
-        0x02, // bmCapabilities
-        // Union Functional Descriptor
-        5,   // bFunctionLength
-        0x24, // bDescriptorType: CS_INTERFACE
-        0x06, // bDescriptorSubtype: Union func desc
-        0x00, // bMasterInterface: Communication class interface
-        0x01, // bSlaveInterface0: Data Class Interface
-        // Endpoint 2 Descriptor
-        7,   // bLength: Endpoint Descriptor size
-        0x05, // USB_ENDPOINT_DESCRIPTOR_TYPE
-        0x82, // bEndpointAddress: (IN2)
-        0x03, // bmAttributes: Interrupt
-        8,   // VIRTUAL_COM_PORT_INT_SIZE
-        0,
-        0xFF, // bInterval:
-        // Data class interface descriptor
-        9,   // bLength: Endpoint Descriptor size
-        0x04, // USB_INTERFACE_DESCRIPTOR_TYPE
-        0x01, // bInterfaceNumber: Number of Interface
-        0x00, // bAlternateSetting: Alternate setting
-        0x02, // bNumEndpoints: Two endpoints used
-        0x0A, // bInterfaceClass: CDC
-        0x00, // bInterfaceSubClass:
-        0x00, // bInterfaceProtocol:
-        0x00, // iInterface:
-        // Endpoint 3 Descriptor
-        7,   // bLength: Endpoint Descriptor size
-        0x05, // USB_ENDPOINT_DESCRIPTOR_TYPE
-        0x03, // bEndpointAddress: (OUT3)
-        0x02, // bmAttributes: Bulk
-        64,  // VIRTUAL_COM_PORT_DATA_SIZE
-        0,
-        0x00, // bInterval: ignore for Bulk transfer
-        // Endpoint 1 Descriptor
-        7,   // bLength: Endpoint Descriptor size
-        0x05, // USB_ENDPOINT_DESCRIPTOR_TYPE
-        0x81, // bEndpointAddress: (IN1)
-        0x02, // bmAttributes: Bulk
-        64,  // VIRTUAL_COM_PORT_DATA_SIZE
-        0,
-        0x00, // bInterval
+        9, 2, 67, 0, 2, 1, 0, 192, 50, // USB Configuration
+        9, 4, 0, 0, 1, 2, 2, 1, 0,     // Interface
+        5, 36, 0, 16, 1,               // Header Functional
+        5, 36, 1, 0, 1,                // Call Management Functional
+        4, 36, 2, 2,                   // ACM Functional
+        5, 36, 6, 0, 1,                // Union Functional
+        7, 5, 130, 3, 8, 0, 255,       // Endpoint 2
+        9, 4, 1, 0, 2, 10, 0, 0, 0,    // Data class interface
+        7, 5, 3, 2, 64, 0, 0,          // Endpoint 3
+        7, 5, 129, 2, 64, 0, 0,        // Endpoint 1
     };
 
     void init () {
-        PinA<12> usbPin;
-        usbPin.mode(Pinmode::out_od);
-        usbPin = 0;
-        wait_ms(5);
-        usbPin = 1;
-        wait_ms(5);
+        PinA<12> usbPin; usbPin.mode(Pinmode::out_od);
+        usbPin = 0; wait_ms(5); usbPin = 1; wait_ms(5);
 
         Port<'A'>::modeMap(0b0001100000000000, Pinmode::alt_out, 10);
 
@@ -155,30 +84,38 @@ namespace USB {
         MMIO32(DCFG) &= ~(0x7F<<4);  // clear DAD
         MMIO32(DCFG) |= (3<<0);  // DSPD
 
-        //while ((MMIO32(GINTSTS) & (1<<12)) == 0) {}  // USBRST
-        //while ((MMIO32(GINTSTS) & (1<<13)) == 0) {}  // ENUMDNE
-
         printf("10 GINTSTS %08x DSTS %08x DCTL %08x\n",
                 MMIO32(GINTSTS), MMIO32(DSTS), MMIO32(DCTL));
+    }
+
+    void sendEp0 (void const* ptr, uint32_t len) {
+        MMIO32(DIEPTSIZ0) = len;
+        MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
+
+        uint32_t const* wptr = (uint32_t const*) ptr;
+        for (uint32_t i = 0; i < len; i += 4)
+            MMIO32(base + 0x1000) = *wptr++;
     }
 
     void poll () {
         uint32_t irq = MMIO32(GINTSTS) & ~0x04008028;
         MMIO32(GINTSTS) = irq;  // clear all interrupts
-        if (irq)
-            printf("irq %08x\n", irq);
+        //if (irq)
+        //    printf("irq %08x\n", irq);
 
         if (irq & (1<<2)) { //  needed?
             printf("GOTGINT %08x\n", MMIO32(GOTGINT));
             MMIO32(GOTGINT) = MMIO32(GOTGINT);
         }
 
+        if (irq & (1<<12))  // USBRST
+            printf("usbrst\n");
+
         if (irq & (1<<13)) {  // ENUMDNE
             printf("enumdne\n");
-#if 0
-            printf("11 GINTSTS %08x DSTS %08x DCTL %08x\n",
-                    MMIO32(GINTSTS), MMIO32(DSTS), MMIO32(DCTL));
-#endif
+            //printf("11 GINTSTS %08x DSTS %08x DCTL %08x\n",
+            //        MMIO32(GINTSTS), MMIO32(DSTS), MMIO32(DCTL));
+
             // see p.1354
             MMIO32(DIEPCTL0) = 0;
             MMIO32(DOEPCTL0) = (1<<15);  // USBAEP
@@ -187,40 +124,31 @@ namespace USB {
             MMIO32(DOEPCTL0 + 0x40) |= (1<<27);  // SNAK ep2
             MMIO32(DOEPCTL0 + 0x60) |= (1<<27);  // SNAK ep3
 
-            MMIO32(DIEPTSIZ0) = 64;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<27);  // EPENA, SNAK
+            //MMIO32(DIEPTSIZ0) = 64;
+            //MMIO32(DIEPCTL0) |= (1<<31) | (1<<27);  // EPENA, SNAK
 
             MMIO32(DOEPTSIZ0) = (3<<29) | (1<<19) | 64;  // STUPCNT, PKTCNT
             MMIO32(DOEPCTL0) |= (1<<31) | (1<<27);  // EPENA, SNAK
 
             MMIO32(GRXFSIZ) = 512/4;  // 512b for receive FIFO
-            MMIO32(DIEPTXF0) = (96/4<<16) | 512;  // then 96b for TX0
+            MMIO32(DIEPTXF0) = (96/4<<16) | 512/4;  // then 96b for TX0
 
             return;
         }
 
         if (irq & (1<<4)) {
-            printf("rx GINTSTS %08x DSTS %08x\n",
-                    MMIO32(GINTSTS), MMIO32(DSTS));
+            //printf("rx GINTSTS %08x DSTS %08x\n",
+            //        MMIO32(GINTSTS), MMIO32(DSTS));
             uint32_t rx = MMIO32(GRXSTSP);
-            uint32_t rxtyp = (rx>>17) & 0xF;
-            printf("GRXSTSP %08x type %x\n", rx, rxtyp);
+            uint32_t typ = (rx>>17) & 0xF;
+            printf("rx %08x typ %x\n", rx, typ);
 
             static union {
-                struct {
-                    uint8_t typ, req;
-                    uint16_t val, idx, len;
-                };
+                struct { uint8_t typ, req; uint16_t val, idx, len; };
                 uint32_t buf [2];
             } setup;
     
-            switch (rxtyp) {
-                case 0b0010:  // IN
-                    printf("in\n");
-                    break;
-                case 0b0011:  // IN complete
-                    printf("in complete\n");
-                    break;
+            switch (typ) {
                 case 0b0110:  // SETUP
                     setup.buf[0] = MMIO32(base + 0x1000);
                     setup.buf[1] = MMIO32(base + 0x1000);
@@ -232,61 +160,44 @@ namespace USB {
                     switch (setup.req) {
                         case 0:  // get status
                             printf("get status\n");
-            MMIO32(DIEPTSIZ0) = 2;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
-            MMIO32(base + 0x1000) = 0;
-                            break;
-                        case 1:  // clear feature
-                            printf("clear feature\n");
-            MMIO32(DIEPTSIZ0) = 0;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
+                            sendEp0("\0\0", 2);
                             break;
                         case 5:  // set address
                             printf("set address %d\n", setup.val);
-                            //MMIO32(DCFG) &= ~(0x7F<<4);
                             MMIO32(DCFG) |= (setup.val<<4);
-            MMIO32(DIEPTSIZ0) = 0;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
+                            sendEp0(0, 0);
                             break;
                         case 6:  // get descriptor
                             printf("get descriptor v %04x i %d #%d\n",
                                     setup.val, setup.len, setup.len);
                             switch (setup.val) {
-                                case 0x100:  // device desc
-            MMIO32(DIEPTSIZ0) = sizeof devDesc;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
-            for (uint32_t i = 0; i < sizeof devDesc; i += 4)
-                MMIO32(base + 0x1000) = ((uint32_t*) &devDesc)[i/4];
+                                case 0x100: {  // device desc
+                                    uint32_t n = sizeof devDesc;
+                                    if (n > setup.len)
+                                        n = setup.len;
+                                    sendEp0(&devDesc, sizeof devDesc);
                                     break;
-                                case 0x200:  // configuration desc
-                                    if (setup.len == 9) {
-            MMIO32(DIEPTSIZ0) = 9;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
-            for (uint32_t i = 0; i < 9; i += 4)
-                MMIO32(base + 0x1000) = ((uint32_t*) confDesc)[i/4];
-                                    } else {
-            MMIO32(DIEPTSIZ0) = sizeof confDesc;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
-            for (uint32_t i = 0; i < sizeof confDesc; i += 4)
-                MMIO32(base + 0x1000) = ((uint32_t*) confDesc)[i/4];
-                                    }
+                                }
+                                case 0x200: {  // configuration desc
+                                    uint32_t n = sizeof confDesc;
+                                    if (n > setup.len)
+                                        n = setup.len;
+                                    sendEp0(confDesc, n);
                                     break;
+                                }
                                 default:
-            MMIO32(DIEPTSIZ0) = 0;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
+                                    sendEp0(0, 0);
                                     break;
                             }
                             break;
                         case 9:  // set configuration
                         //case 32:  // set line coding
-            MMIO32(DIEPTSIZ0) = 0;
-            MMIO32(DIEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
+                            sendEp0(0, 0);
                             break;
                     }
                     break;
             }
 
-            //MMIO32(DOEPTSIZ0) = 64;
             MMIO32(DOEPTSIZ0) = (3<<29) | (1<<19) | 64;  // STUPCNT, PKTCNT
             MMIO32(DOEPCTL0) |= (1<<31) | (1<<26);  // EPENA, CNAK
         }
