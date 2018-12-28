5d4e2c87f38837af24196cae0670b66a3637dcf8
Jean-Claude Wippler <jc@wippler.nl>
2018-12-28 14:03:31 +0100

    add nucleo-l053 blink demo

---
+++ nucleo-l053/blink/src/main.cpp
@@ -0,0 +1,24 @@
+#include <jee.h>
+
+UartBufDev< PinA<2>, PinA<3> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+    return 0;
+}
+
+PinA<5> led;
+
+int main() {
+    console.init();
+    console.baud(115200, fullSpeedClock());
+    led.mode(Pinmode::out);
+
+    while (true) {
+        printf("%d\n", ticks);
+        led = 1;
+        wait_ms(100);
+        led = 0;
+        wait_ms(400);
+    }
+}
