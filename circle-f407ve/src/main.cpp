b0756faca8d3f0064af0124259547802d5e73130
Jean-Claude Wippler <jc@wippler.nl>
2019-04-25 23:57:43 +0200

    explore setjmp/longjmp for task switching

--- circle-f407ve/src/main.cpp
+++ circle-f407ve/src/main.cpp
@@ -6,6 +6,7 @@
 // when started, the app in lomem jumps to the main entry of the app in himem
 
 #include <jee.h>
+#include <setjmp.h>
 
 void(**linkArea)() = (void(**)()) 0x10004000;
 
@@ -23,10 +24,58 @@ static void toggleLed () {
     led.toggle();
 }
 
+// This code helps figure out how to re-use the setjmp/longjmp mechanism for
+// task switching - all we need are the offsets where SP and PC are stored.
+
+jmp_buf jb;
+
+void dumpJump (bool immediate) {
+    if (immediate) {
+        setjmp(jb); // fill in jb
+
+        uint32_t sp = (uint32_t) &immediate; // the real SP is slightly lower
+        uint32_t pc = (uint32_t) &dumpJump;  // the real PC is slightly higher
+        printf("'setjmp' layout guess: sp < %08x, pc > %08x:", sp, pc);
+
+        for (int i = 0; i < sizeof jb / 4; ++i) {
+            if (i % 8 == 0)
+                printf("\n%6d: ", i);
+            printf(" %08x", ((uint32_t*) &jb)[i]);
+        }
+        printf("\n");
+
+        // find the most plausible location of sp in the jb buffer
+        for (int i = 0; i < sizeof jb / 4; ++i)
+            if (sp - ((uint32_t*) &jb)[i] < 10)
+                printf("  best guess for SP is word #%d (%d off)\n",
+                        i, sp - ((uint32_t*) &jb)[i]);
+
+        // find the most plausible location of sp in the jb buffer
+        for (int i = 0; i < sizeof jb / 4; ++i)
+            if (((uint32_t*) &jb)[i] - pc < 30)
+                printf("  best guess for PC is word #%d (%d off)\n",
+                        i, ((uint32_t*) &jb)[i] - pc);
+        return;
+    }
+    
+    // else use recursion to add an extra stack frame
+    dumpJump(!immediate);
+}
+
+void jumpInfo () {
+    printf("FIRST ");
+    dumpJump(true);
+    printf("SECOND ");
+    dumpJump(false);
+    printf("sizeof (jmp_buf) = %d bytes\n", sizeof (jmp_buf));
+}
+
 int main() {
     console.init();
     enableSysTick();
     led.mode(Pinmode::out);
+
+    jumpInfo();
 
     *linkArea = toggleLed;
 
