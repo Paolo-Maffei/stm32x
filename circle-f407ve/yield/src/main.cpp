4001c9cfef451e3a296830d0881f0df01d2b381c
Jean-Claude Wippler <jc@wippler.nl>
2019-04-26 13:57:48 +0200

    simplify

--- circle-f407ve/yield/src/main.cpp
+++ circle-f407ve/yield/src/main.cpp
@@ -47,7 +47,8 @@ void launch (void (*proc)(), uint32_t* stack, jmp_buf jbp) {
     memset(jbp, 0, sizeof jbp);
     ((uint32_t*) jbp)[8] = (uint32_t) (stack + 100);
     ((uint32_t*) jbp)[9] = (uint32_t) proc;
-    longjmp(jbp, 1);
+    if (setjmp(jbSys) == 0)
+        longjmp(jbp, 1);
 }
 
 int main() {
@@ -55,10 +56,8 @@ int main() {
     console.baud(115200, fullSpeedClock()/2);
     led.mode(Pinmode::out);
 
-    if (setjmp(jbSys) == 0)
-        launch(taskOne, stackOne, jbOne);
-    if (setjmp(jbSys) == 0)
-        launch(taskTwo, stackTwo, jbTwo);
+    launch(taskOne, stackOne, jbOne);
+    launch(taskTwo, stackTwo, jbTwo);
 
     while (true) {
         printf("t = %d\n", ticks);
