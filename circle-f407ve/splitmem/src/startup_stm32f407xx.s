dff313eedc3b021377303f0fe7757d9f732466dd
Jean-Claude Wippler <jc@wippler.nl>
2019-05-04 14:34:49 +0200

    oops, too much editing ...

--- circle-f407ve/splitmem/src/startup_stm32f407xx.s
+++ circle-f407ve/splitmem/src/startup_stm32f407xx.s
@@ -21,6 +21,25 @@
 Reset_Handler:  
   ldr   sp, =_estack     /* set stack pointer */
 
+#if LOMEM
+/* Copy the data segment initializers from flash to SRAM */  
+  movs  r1, #0
+  b  LoopCopyDataInit
+
+CopyDataInit:
+  ldr  r3, =_sidata
+  ldr  r3, [r3, r1]
+  str  r3, [r0, r1]
+  adds  r1, r1, #4
+    
+LoopCopyDataInit:
+  ldr  r0, =_sdata
+  ldr  r3, =_edata
+  adds  r2, r0, r1
+  cmp  r2, r3
+  bcc  CopyDataInit
+#endif
+
   ldr  r2, =_sbss
   b  LoopFillZerobss
 /* Zero fill the bss segment. */  
@@ -32,6 +51,11 @@ LoopFillZerobss:
   ldr  r3, = _ebss
   cmp  r2, r3
   bcc  FillZerobss
+
+#if LOMEM
+/* Call the clock system intitialization function.*/
+  bl  SystemInit   
+#endif
 
 /* Call static constructors */
     bl __libc_init_array
