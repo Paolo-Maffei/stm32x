78590c62acbfc9ae02eec3c0ca4a0ad97d39e2b8
Jean-Claude Wippler <jc@wippler.nl>
2020-03-15 00:00:01 +0100

    merge driver-specific details into driver

--- nucleo-l031/rfm69/src/main.cpp
+++ nucleo-l031/rfm69/src/main.cpp
@@ -1,4 +1,5 @@
 #include <jee.h>
+#include <jee/spi-rf69.h>
 
 UartBufDev< PinA<2>, PinA<15> > console;
 
@@ -7,11 +8,8 @@ int printf(const char* fmt, ...) {
     return 0;
 }
 
-#include <jee/spi-rf69.h>
-
 PinA<0> dio0;
 PinA<8> dio3;
-PinB<1> rfrst;
 
 SpiHw< PinB<5>, PinB<4>, PinB<3>, PinA<11> > spi;  // for Nucleo-32
 RF69< decltype(spi) > rf;
@@ -25,12 +23,13 @@ int main() {
     dio3.mode(Pinmode::in_float);
 
     // RFM69 hard reset, so this code always starts from a known state
+    PinB<1> rfrst;
     rfrst.mode(Pinmode::out);
     rfrst = 1; wait_ms(2); rfrst = 0; wait_ms(10);
 
     spi.init();
     rf.init(63, 42, 8683);  // node 63, group 42, 868.3 MHz
-    rf.txPower(6);
+    rf.txPower(3);
 
     // prepare DIO pins for GPIO polling, instead of the RF69's SPI polling
     constexpr int DIO0 = 1, DIO1 = 3, DIO2 = 3, DIO3 = 2, DIO5 = 3;//1;
@@ -40,7 +39,6 @@ int main() {
     // the following loop is a bit convoluted, because it is mostly checking
     // for new incoming packets, but it also sends out a packet once a second
 
-    int seq = 0;
     while (true) {
         // when waiting for a new incoming packet, don't use SPI polling
         // instead, poll the DIO0 and DIO3 pins (will use interrupts later)
@@ -48,25 +46,21 @@ int main() {
         while (!dio3 && ticks % 1000 != 0) {} // wait for DIO3 0 => 1
 
         if (!dio3 && ticks % 1000 == 0) {
+            static int seq = 0;
             rf.send(0, &seq, sizeof seq);
             ++seq;
             while (!dio0) {} // wait for packet sent to start
             while (dio0) {} // wait for packet sent to complete
-            rf.writeReg(0x3B, 0b01100101); // standby mode until fifo empty
-            rf.setMode(rf.MODE_RECEIVE);
+            rf.resume();
             continue;
         }
 
-        // fetch rssi, lnd, and afc
-        rf.rssi = rf.readReg(rf.REG_RSSIVALUE);
-        rf.lna = (rf.readReg(rf.REG_LNAVALUE) >> 3) & 0x7;
-        rf.afc = rf.readReg(rf.REG_AFCMSB) << 8;
-        rf.afc |= rf.readReg(rf.REG_AFCLSB);
+        rf.rssiCapture();
 
         while (dio3 && !dio0) {} // wait for DIO3 == 1 && DIO0 1 => 0
 
         uint8_t rxBuf [66];
-        auto rxLen = rf.receive(rxBuf, sizeof rxBuf);
+        int rxLen = rf.receive(rxBuf, sizeof rxBuf);
 
         if (rxLen >= 0) {
             printf("RF69 #%d: ", rxLen);
@@ -75,7 +69,7 @@ int main() {
                 if (i < 2)
                     printf(" ");
             }
-            printf(" r%4d l%2d a%4d\n", rf.rssi, rf.lna, rf.afc);
+            printf(" r%4d l%2d a %d\n", rf.rssi, rf.lna, rf.afc);
         }
     }
 }
