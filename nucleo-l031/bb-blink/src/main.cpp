5bd0ab6d3abea35473f106b6ba11cf80afea74ce
Jean-Claude Wippler <jc@wippler.nl>
2020-03-17 00:38:43 +0100

    add breadboard L031 tssop-20 blink demo

---
+++ nucleo-l031/bb-blink/src/main.cpp
@@ -0,0 +1,25 @@
+#include <jee.h>
+
+UartBufDev< PinA<2>, PinA<15> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+    return 0;
+}
+
+PinA<1> led;
+
+int main() {
+    console.init();
+    //console.baud(115200, fullSpeedClock());
+    enableSysTick();
+    led.mode(Pinmode::out);
+
+    while (true) {
+        printf("%d\n", ticks);
+        led = 1;
+        wait_ms(100);
+        led = 0;
+        wait_ms(400);
+    }
+}
