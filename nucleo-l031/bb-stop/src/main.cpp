37fe072f83fa5521be5e702f6fdb54fa7101fb29
Jean-Claude Wippler <jc@wippler.nl>
2020-03-18 17:51:13 +0100

    add radio, immediately put to sleep

--- nucleo-l031/bb-stop/src/main.cpp
+++ nucleo-l031/bb-stop/src/main.cpp
@@ -1,6 +1,13 @@
 #include <jee.h>
+#include <jee/spi-rf69.h>
+
+SpiHw< PinA<7>, PinA<6>, PinA<5>, PinA<4> > spi;
+RF69< decltype(spi) > rf;
 
 int main() {
+    spi.init(0); // div=0 @ 2 MHz: 1 Mhz
+    rf.init(63, 42, 8683);  // node 63, group 42, 868.3 MHz
+
     while (true)
         powerDown(true);
 }
