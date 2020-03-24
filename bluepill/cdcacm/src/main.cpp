ea94687f3465f61208755c29d9e1c0d6f67f06b4
Jean-Claude Wippler <jc@wippler.nl>
2020-03-25 00:25:55 +0100

    working usb <=> serial bridge on Blue Pill

--- bluepill/cdcacm/src/main.cpp
+++ bluepill/cdcacm/src/main.cpp
@@ -18,13 +18,62 @@ usbd_device* setupUsb (); // in src/usb.c
 void cdcacm_data_rx_cb (usbd_device*, uint8_t);
 }
 
-void cdcacm_data_rx_cb (usbd_device* dev, uint8_t) {
-    char buf[64];
-    int len = usbd_ep_read_packet(dev, 0x01, buf, sizeof buf);
+struct XUsbDev {
+    RingBuffer<100> recv;
+    uint8_t xmitBuf [64];
+    uint8_t xmitFill = 0;
+    usbd_device* dev = 0;
 
-    // this approach is flawed: data is lost when it can't all be sent at once
-    if (len)
-        usbd_ep_write_packet(dev, 0x82, buf, len);
+    void init () {
+        dev = setupUsb();
+    }
+
+    bool writable () {
+        poll();
+        return xmitFill < sizeof xmitBuf;
+    }
+
+    void putc (int c) {
+        while (!writable()) {}
+        xmitBuf[xmitFill++] = c;
+    }
+
+    bool readable () {
+        poll();
+        return recv.avail() > 0;
+    }
+
+    int getc () {
+        while (!readable()) {}
+        return recv.get();
+    }
+
+    void poll () {
+        usbd_poll(dev);
+
+	// if there's data to send, try doing so
+	if (xmitFill > 0) {
+            int n = usbd_ep_write_packet(dev, 0x82, xmitBuf, xmitFill);
+            if (n > 0) // TODO make sure n == xmitFill!
+                xmitFill = 0;
+	}
+    }
+
+    void callback () {
+        // there is room if less than 30 of 100 slots are used
+        if (recv.avail() < 30) {
+            uint8_t tmpBuf [64];
+            int n = usbd_ep_read_packet(dev, 0x01, tmpBuf, sizeof tmpBuf);
+                for (int i = 0; i < n; ++i)
+                    recv.put(tmpBuf[i]);
+        }
+    }
+};
+
+XUsbDev usb;
+
+void cdcacm_data_rx_cb (usbd_device* dev, uint8_t) {
+    usb.callback();
 }
 
 int main () {
@@ -41,7 +90,11 @@ int main () {
     wait_ms(2);
     usbPin.mode(Pinmode::in_float);
 
-    usbd_device* dev = setupUsb();
-    while (1)
-        usbd_poll(dev);
+    usb.init();
+    while (true) {
+        if (console.readable() && usb.writable())
+            usb.putc(console.getc());
+        if (usb.readable() && console.writable())
+            console.putc(usb.getc());
+    }
 }
