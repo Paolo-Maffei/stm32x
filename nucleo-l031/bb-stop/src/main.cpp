cfd261fa27995c83b6e80c1a05924541e6e48c04
Jean-Claude Wippler <jc@wippler.nl>
2020-03-30 10:24:53 +0200

    high current draw was caused by MISO pin

--- nucleo-l031/bb-stop/src/main.cpp
+++ nucleo-l031/bb-stop/src/main.cpp
@@ -8,15 +8,15 @@ RTC rtc;
 int main () {
     rtc.init(true);
 
+    // the RFM's reset is not used here, it does not need to be initialised
+    //PinB<1> rst; rst=0; rst.mode(Pinmode::out); // rfm reset, active high
+
     spi.init(0); // div=0 @ 2 MHz: 1 Mhz
     rf.init(63, 42, 8683);  // node 63, group 42, 868.3 MHz
 
-    PinA<4> p4; p4=1; p4.mode(Pinmode::in_pullup);
-    PinA<5> p5; p5=1; p5.mode(Pinmode::in_pullup);
-    PinA<6> p6; p6=1; p6.mode(Pinmode::in_pullup);
-    PinA<7> p7; p7=1; p7.mode(Pinmode::in_pullup);
-
-    PinB<1> pR; pR=0; pR.mode(Pinmode::out); // rfm reset, active high
+    // don't let the MISO pin float, it will draw over 50 µA @ 2.7V (!)
+    // this will need to be restored to Pinmode::out_od after resuming
+    PinA<6> miso; miso=1; miso.mode(Pinmode::in_pullup);
 
     powerDown(false);
 
