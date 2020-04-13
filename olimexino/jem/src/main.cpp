b71c073197ecbe02dffff77e49ef50f348f28f82
Jean-Claude Wippler <jc@wippler.nl>
2020-04-13 19:52:21 +0200

    implement 4-chan ADC acquisition using DMA

--- olimexino/jem/src/main.cpp
+++ olimexino/jem/src/main.cpp
@@ -15,9 +15,37 @@ PinC<2> ct3;
 PinA<0> acin;
 
 ADC<1> adc;
+Timer<3> timer;
+
+constexpr int NSAMP = 2000, NCHAN = 4;
+uint16_t adcData [NSAMP][NCHAN];
 
 SpiHw< PinA<7>, PinA<6>, PinA<5>, PinA<4> > spi;
 RF69< decltype(spi) > rf;
+
+void dmaCapture () {
+    constexpr static uint32_t dma1  = 0x40020000;
+
+    Periph::bit(Periph::rcc+0x14, 0) = 1;     // DMA1EN
+    MMIO32(dma1+0x0C) = NSAMP*NCHAN;          // CNDTR1
+    MMIO32(dma1+0x14) = (uint32_t) adcData;   // CMAR1
+    MMIO32(dma1+0x10) = adc.dr;               // CPAR1
+    MMIO32(dma1+0x08) = (1<<10) | (1<<8) | (1<<7) | (1<<5) | (1<<0); // CCR1
+
+    MMIO32(adc.cr2) = (1<<20) | (4<<17) | (1<<8) | (1<<0); // config for DMA
+
+    // configure 4-chan acquisition
+    MMIO32(adc.smpr1) = (3<<9) | (3<<6) | (3<<3) | (3<<0); // 28.5+12.5 cycles
+    MMIO32(adc.base+0x2C) = (3<<20);                               // SQR1
+    MMIO32(adc.base+0x34) = (0<<15) | (12<10) | (11<<5) | (10<<0); // SQR3
+    Periph::bit(adc.cr1, 8) = 1;                                   // SCAN
+
+    // wait for the DMA capture to complete
+    while (Periph::bit(dma1+0x00, 1) == 0) {} // TCIF1 in ISR
+    Periph::bit(dma1+0x04, 0) = 1;            // CGIF1 in IFCR
+
+    Periph::bit(Periph::rcc+0x14, 0) = 0;     // DMA1EN
+}
 
 int main() {
     (void) powerDown;
@@ -26,21 +54,24 @@ int main() {
     console.init();
     console.baud(115200, fullSpeedClock());
     led.mode(Pinmode::out);
+    wait_ms(100);
 
-    adc.init();
     spi.init();
     //rf.init(63, 42, 8683);
     //rf.txPower(0);
 
+    timer.init(16, 36-1); // 16 µs, 36 MHz APB1
+    adc.init();
+
     while (true) {
-        int aVal = adc.read(ct1);
-#if 0
-        printf("%d %d\n", ticks, aVal);
-#else
-        for (int i = 0; i < aVal; i += 60)
-            console.putc('-');
-        printf("+\n");
-#endif
-        wait_ms(10);
+        wait_ms(1000);
+
+        uint32_t t = ticks;
+        dmaCapture();
+        t = ticks - t;
+        for (int i = 0; i < 10; ++i)
+            for (int j = 0; j < NCHAN; ++j)
+                printf("%d%c", adcData[i][j], j < NCHAN-1 ? ',' : '\n');
+        printf("%d\n", t);
     }
 }
