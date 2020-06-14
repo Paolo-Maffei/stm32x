8cc92b2f0097daf84c5520295507981d2b65b761
Jean-Claude Wippler <jc@wippler.nl>
2020-06-14 16:41:24 +0200

    fix indentation

--- diymore-f407vg/blink/src/main.cpp
+++ diymore-f407vg/blink/src/main.cpp
@@ -6,7 +6,7 @@ UartDev< PinA<9>, PinA<10> > console;
 
 int printf(const char* fmt, ...) {
     va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
-	return 0;
+    return 0;
 }
 
 PinE<0> led;
