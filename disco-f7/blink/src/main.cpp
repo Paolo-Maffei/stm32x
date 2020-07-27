7ffab1c7b28cfc997b58c763e3619022db3f1fea
Jean-Claude Wippler <jc@wippler.nl>
2020-07-28 00:03:24 +0200

    UARTs fixed, but fullSpeedClock() needs work for STM32F7

--- disco-f7/blink/src/main.cpp
+++ disco-f7/blink/src/main.cpp
@@ -1,7 +1,6 @@
 #include <jee.h>
 
-//UartBufDev< PinA<2>, PinA<3> > console;
-UartDev< PinA<9>, PinA<10> > console;
+UartBufDev< PinA<9>, PinB<7> > console;
 
 int printf(const char* fmt, ...) {
     va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
@@ -12,7 +11,6 @@ PinK<3> led;
 
 int main() {
     console.init();
-    //printf("123\n");
     //console.baud(115200, fullSpeedClock());
     enableSysTick();
 
@@ -20,7 +18,7 @@ int main() {
     led = 0;
 
     while (1) {
-        //printf("%d\n", ticks);
+        printf("%d\n", ticks);
         led = 1;
         wait_ms(100);
         led = 0;
