08bfe9e0e067453c215cf097f99c9782662dceb7
Jean-Claude Wippler <jc@wippler.nl>
2020-07-27 22:23:18 +0200

    add WaveShare OpenH7xx blink/uart test

---
+++ openh7xx/blink/src/main.cpp
@@ -0,0 +1,28 @@
+#include <jee.h>
+
+UartBufDev< PinA<9>, PinA<10> > console;
+
+int printf(const char* fmt, ...) {
+    va_list ap; va_start(ap, fmt); veprintf(console.putc, fmt, ap); va_end(ap);
+    return 0;
+}
+
+PinB<6> led;
+
+int main() {
+    console.init();
+    console.baud(115200, fullSpeedClock()/4);
+    led.mode(Pinmode::out);
+
+    PinA<3> backlight;
+    backlight.mode(Pinmode::out);
+    backlight = 1; // turn LCD backlight off
+
+    while (true) {
+        printf("%d\n", ticks);
+        led = 1;
+        wait_ms(100);
+        led = 0;
+        wait_ms(400);
+    }
+}
